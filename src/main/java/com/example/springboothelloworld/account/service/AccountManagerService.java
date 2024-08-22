da27c1e5a27b8d2c0c76142c14e2faac00fb2861
Justin Orr <justin.jorr1@gmail.com>
2024-08-22 16:04:07 -0500

    Add AccountManagerService

---
+++ src/main/java/com/example/springboothelloworld/account/service/AccountManagerService.java
@@ -0,0 +1,38 @@
+package com.example.springboothelloworld.account.service;
+
+import com.example.springboothelloworld.account.domain.Account;
+import com.example.springboothelloworld.account.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+@Service
+public class AccountManagerService {
+
+    private final AccountRepository accountRepository;
+    private static int idCounter = 1;
+
+    public AccountManagerService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public boolean createAccount(String name) {
+        return accountRepository.insertAccount(new Account(idCounter++, name));
+    }
+
+    public Account getAccount(int id) {
+        return accountRepository.findAccount(id);
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accountRepository.getAccounts();
+    }
+
+    public boolean updateAccount(Account account) {
+        return accountRepository.updateAccount(account);
+    }
+
+    public boolean deleteAccount(Account account) {
+        return accountRepository.deleteAccount(account);
+    }
+
+}
